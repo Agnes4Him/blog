506f1e72cb7cddc2c035aebc95100f16e17f9a04
Agnes4Him <ojuhagnes@gmail.com>
2022-02-01 02:48:02 -0800

    A message

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const dbUrl = process.env.CONNECT_KEY
 
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs')
 
 app.use(express.json())
